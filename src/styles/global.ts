ccd993a2addcc391e7c5362bc1e9691941a02bdd
francisco-josé-vieira <franciscojose2703@gmail.com>
2025-02-06 14:24:42 -0300

    Importei as fontes

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import {createGlobalStyle} from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     :root {
@@ -17,6 +17,15 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+    }
+
+    body, input, textarea, button {
+        font-family: "Poppins", sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
     }
 
     html {
@@ -44,4 +53,4 @@ export const GlobalStyle = createGlobalStyle`
     }
 
 
-`
+`;
